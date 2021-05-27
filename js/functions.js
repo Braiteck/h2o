9ce90dbc9891926f104b019f7188d62431b4c888
Vitaliy Sulimenko <prevetal@gmail.com>
2021-05-27 11:58:54 +0300

    Новые блоки на главной

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
 	// Проверка браузера
-	if ( !supportsCssVars() ) {
+	if (!supportsCssVars()) {
 		$('body').html('<div style="text-align: center; padding: 30px; font-family: Arial, sans-serif;">Ваш браузер устарел рекомендуем обновить его до последней версии<br> или использовать другой более современный</div>')
 	}
 
@@ -20,72 +20,72 @@ $(function(){
 	$('select').niceSelect()
 
 	// Фокус при клике на название поля
-	$('body').on('click', '.form .label', function() {
-    	$(this).next().find('.input, textarea').focus()
+	$('body').on('click', '.form .label', function () {
+		$(this).next().find('.input, textarea').focus()
 	})
 
 	// Выбор файла
-	$('body').on('change', '.form input[type=file]', function(e) {
-    	$(this).next().find('.path').text( $(this).val() )
+	$('body').on('change', '.form input[type=file]', function (e) {
+		$(this).next().find('.path').text($(this).val())
 	})
 
 
 	// Мини всплывающие окна
 	firstClick = false
 
-	$('.mini_modal_link').click(function(e){
-	    e.preventDefault()
+	$('.mini_modal_link').click(function (e) {
+		e.preventDefault()
 
-	    let modalId = $(this).data('modal-id')
+		let modalId = $(this).data('modal-id')
 
-	    if( $(this).hasClass('active') ){
-	        $(this).removeClass('active')
-	      	$('.mini_modal').removeClass('active')
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
 
-	        firstClick = false
+			firstClick = false
 
-			if( $(window).width() < 1024 ){
+			if ($(window).width() < 1024) {
 				$('body').css('cursor', 'default')
 			}
-	    }else{
-	        $('.mini_modal_link').removeClass('active')
-	        $(this).addClass('active')
+		} else {
+			$('.mini_modal_link').removeClass('active')
+			$(this).addClass('active')
 
-	        $('.mini_modal').removeClass('active')
-	        $(modalId).addClass('active')
+			$('.mini_modal').removeClass('active')
+			$(modalId).addClass('active')
 
-	        firstClick = true
+			firstClick = true
 
-			if( $(window).width() < 1024 ){
+			if ($(window).width() < 1024) {
 				$('body').css('cursor', 'pointer')
 			}
-	    }
+		}
 	})
 
 	// Закрываем всплывашку при клике за её пределами
-	$(document).click(function(e){
-	    if ( !firstClick && $(e.target).closest('.mini_modal').length == 0 ){
-	        $('.mini_modal, .mini_modal_link').removeClass('active')
+	$(document).click(function (e) {
+		if (!firstClick && $(e.target).closest('.mini_modal').length == 0) {
+			$('.mini_modal, .mini_modal_link').removeClass('active')
 
-			if( $(window).width() < 1024 ){
+			if ($(window).width() < 1024) {
 				$('body').css('cursor', 'default')
 			}
-	    }
+		}
 
-	    firstClick = false
+		firstClick = false
 	})
 
 	// Закрываем всплывашку при клике на крестик во всплывашке
-	$('body').on('click', '.mini_modal .close', function(e) {
-	    e.preventDefault()
+	$('body').on('click', '.mini_modal .close', function (e) {
+		e.preventDefault()
 
-	    $('.mini_modal, .mini_modal_link').removeClass('active')
+		$('.mini_modal, .mini_modal_link').removeClass('active')
 
-	    if( $(window).width() < 1024 ){
+		if ($(window).width() < 1024) {
 			$('body').css('cursor', 'default')
 		}
 
-	    firstClick = false
+		firstClick = false
 	})
 
 
@@ -99,7 +99,7 @@ $(function(){
 	$.fancybox.defaults.speed = 500
 	$.fancybox.defaults.gutter = 40
 	$.fancybox.defaults.i18n = {
-		'en' : {
+		'en': {
 			CLOSE: "Закрыть",
 			NEXT: "Следующий",
 			PREV: "Предыдущий",
@@ -116,19 +116,19 @@ $(function(){
 
 
 	// Всплывающие окна
-	$('body').on('click', '.modal_link', function(e) {
+	$('body').on('click', '.modal_link', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close(true)
 
 		$.fancybox.open({
-			src  : $(this).attr('href'),
-			type : 'inline'
+			src: $(this).attr('href'),
+			type: 'inline'
 		})
 	})
 
 	// Закрытие всплывающего окна по произвольной кнопке
-	$('body').on('click', '.modal .close', function(e) {
+	$('body').on('click', '.modal .close', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close(true)
@@ -140,69 +140,69 @@ $(function(){
 
 
 	// Изменение количества товара
-	$('body').on('click', '.amount .minus', function(e) {
-	    e.preventDefault()
+	$('body').on('click', '.amount .minus', function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.amount')
-	    let input = parent.find('input')
-	    let inputVal = parseFloat( input.val() )
-	    let minimum = parseFloat( input.data('minimum') )
-	    let step = parseFloat( input.data('step') )
+		let parent = $(this).closest('.amount')
+		let input = parent.find('input')
+		let inputVal = parseFloat(input.val())
+		let minimum = parseFloat(input.data('minimum'))
+		let step = parseFloat(input.data('step'))
 
-	    if( inputVal > minimum ) {
-	    	input.val( inputVal-step )
-	    }
+		if (inputVal > minimum) {
+			input.val(inputVal - step)
+		}
 
-	    if( $(this).closest('.product_info') ) {
-	    	let price = $(this).closest('.product_info').find('.price').data('price')
-	    	let totalPrice = price*(inputVal-step)
+		if ($(this).closest('.product_info')) {
+			let price = $(this).closest('.product_info').find('.price').data('price')
+			let totalPrice = price * (inputVal - step)
 
-	    	$('.product_info .info .price .total .count').text( inputVal-step )
-	    	$('.product_info .info .price .total .val').text( totalPrice )
-	    }
+			$('.product_info .info .price .total .count').text(inputVal - step)
+			$('.product_info .info .price .total .val').text(totalPrice)
+		}
 	})
 
-	$('body').on('click', '.amount .plus', function(e) {
-	    e.preventDefault()
+	$('body').on('click', '.amount .plus', function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.amount')
-	    let input = parent.find('input')
-	    let inputVal = parseFloat( input.val() )
-	    let maximum = parseFloat( input.data('maximum') )
-	    let step = parseFloat( input.data('step') )
+		let parent = $(this).closest('.amount')
+		let input = parent.find('input')
+		let inputVal = parseFloat(input.val())
+		let maximum = parseFloat(input.data('maximum'))
+		let step = parseFloat(input.data('step'))
 
-	    if( inputVal < maximum ) {
-	    	input.val( inputVal+step )
-	    }
+		if (inputVal < maximum) {
+			input.val(inputVal + step)
+		}
 
-	    if( $(this).closest('.product_info') ) {
-	    	let price = $(this).closest('.product_info').find('.price').data('price')
-	    	let totalPrice = price*(inputVal+step)
+		if ($(this).closest('.product_info')) {
+			let price = $(this).closest('.product_info').find('.price').data('price')
+			let totalPrice = price * (inputVal + step)
 
-	    	$('.product_info .info .price .total .count').text( inputVal+step )
-	    	$('.product_info .info .price .total .val').text( totalPrice )
-	    }
+			$('.product_info .info .price .total .count').text(inputVal + step)
+			$('.product_info .info .price .total .val').text(totalPrice)
+		}
 	})
 
 
-	$('body').on('keydown', '.product_info .amount .input', function() {
+	$('body').on('keydown', '.product_info .amount .input', function () {
 		var _self = $(this)
 		var price = $(this).closest('.product_info').find('.price').data('price')
 
-		setTimeout(function(){
-			$('.product_info .info .price .total .count').text( _self.val() )
-	    	$('.product_info .info .price .total .val').text( price*parseInt(_self.val()) )
+		setTimeout(function () {
+			$('.product_info .info .price .total .count').text(_self.val())
+			$('.product_info .info .price .total .val').text(price * parseInt(_self.val()))
 		}, 10)
 	})
 
 
 	// Аккордион
-	$('body').on('click', '.accordion .item .title', function(e) {
-    	e.preventDefault()
+	$('body').on('click', '.accordion .item .title', function (e) {
+		e.preventDefault()
 
-    	let parent = $(this).closest('.accordion')
+		let parent = $(this).closest('.accordion')
 
-		if( $(this).hasClass('active') ) {
+		if ($(this).hasClass('active')) {
 			$(this).removeClass('active').next().slideUp(300)
 		} else {
 			parent.find('.item .title').removeClass('active')
@@ -210,55 +210,91 @@ $(function(){
 
 			$(this).addClass('active').next().slideDown(300)
 		}
-    })
+	})
+
+
+	// Табы
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs button', function (e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			const $parent = $(this).closest('.tabs_container'),
+				activeTab = $(this).data('content'),
+				$activeTabContent = $(activeTab),
+				level = $(this).data('level')
+
+			$parent.find('.tabs:first button').removeClass('active')
+			$parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			$activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		const $activeTab = $('.tabs button[data-content=' + locationHash + ']'),
+			$activeTabContent = $(locationHash),
+			$parent = $activeTab.closest('.tabs_container'),
+			level = $activeTab.data('level')
+
+		$parent.find('.tabs:first button').removeClass('active')
+		$parent.find('.tab_content.' + level).removeClass('active')
+
+		$activeTab.addClass('active')
+		$activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
 
 
 	// Моб. меню
-	$('body').on('click', 'header .mob_menu_link', function(e) {
-    	e.preventDefault()
+	$('body').on('click', 'header .mob_menu_link', function (e) {
+		e.preventDefault()
 
-		if( $(this).hasClass('active') ) {
+		if ($(this).hasClass('active')) {
 			$(this).removeClass('active')
 
-        	$('header .links, header .menu').fadeOut(200)
+			$('header .links, header .menu').fadeOut(200)
 		} else {
 			$(this).addClass('active')
 
 			$('header .links, header .menu').fadeIn(300)
 		}
-    })
+	})
 
-    if( is_touch_device() ){
-    	$('header .menu .item > a.sub_link').addClass('touch_link')
+	if (is_touch_device()) {
+		$('header .menu .item > a.sub_link').addClass('touch_link')
 
-    	$('body').on('click', 'header .menu .item > a.touch_link', function(e) {
-    		if( $(this).next().css('visibility') == 'hidden' ) {
-	    		e.preventDefault()
+		$('body').on('click', 'header .menu .item > a.touch_link', function (e) {
+			if ($(this).next().css('visibility') == 'hidden') {
+				e.preventDefault()
 
 				$('header .menu .sub_menu').removeClass('show')
 
 				$(this).next().addClass('show')
-    		}
-	    })
-    }
+			}
+		})
+	}
 })
 
 
 
 // Вспомогательные функции
-function setHeight(className){
-    let maxheight = 0
-    let object = $(className)
+function setHeight(className) {
+	let maxheight = 0
+	let object = $(className)
 
-    object.each(function() {
-    	let elHeight = $(this).innerHeight()
+	object.each(function () {
+		let elHeight = $(this).innerHeight()
 
-        if( elHeight > maxheight ) {
-        	maxheight = elHeight
-        }
-    })
+		if (elHeight > maxheight) {
+			maxheight = elHeight
+		}
+	})
 
-    object.innerHeight( maxheight )
+	object.innerHeight(maxheight)
 }
 
 
@@ -268,28 +304,28 @@ function is_touch_device() {
 
 
 function widthScroll() {
-    let div = document.createElement('div')
-    div.style.overflowY = 'scroll'
-    div.style.width = '50px'
-    div.style.height = '50px'
-    div.style.visibility = 'hidden'
-    document.body.appendChild(div)
+	let div = document.createElement('div')
+	div.style.overflowY = 'scroll'
+	div.style.width = '50px'
+	div.style.height = '50px'
+	div.style.visibility = 'hidden'
+	document.body.appendChild(div)
 
-    let scrollWidth = div.offsetWidth - div.clientWidth
-    document.body.removeChild(div)
+	let scrollWidth = div.offsetWidth - div.clientWidth
+	document.body.removeChild(div)
 
-    return scrollWidth
+	return scrollWidth
 }
 
 
-var supportsCssVars = function() {
-    var s = document.createElement('style'),
-        support
+var supportsCssVars = function () {
+	var s = document.createElement('style'),
+		support
 
-    s.innerHTML = ":root { --tmp-var: bold; }"
-    document.head.appendChild(s)
-    support = !!(window.CSS && window.CSS.supports && window.CSS.supports('font-weight', 'var(--tmp-var)'))
-    s.parentNode.removeChild(s)
+	s.innerHTML = ":root { --tmp-var: bold; }"
+	document.head.appendChild(s)
+	support = !!(window.CSS && window.CSS.supports && window.CSS.supports('font-weight', 'var(--tmp-var)'))
+	s.parentNode.removeChild(s)
 
-    return support
+	return support
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 	// Основной слайдер на главной
 	$('.main_slider .slider').owlCarousel({
 		items: 1,
@@ -14,16 +14,16 @@ $(function(){
 
 	// Слайдер в тексте
 	$('.text_block .slider_in_text .big .slider').owlCarousel({
-	    items: 1,
+		items: 1,
 		margin: 20,
 		loop: false,
 		nav: false,
 		dots: false,
 		smartSpeed: 500,
-	    onTranslate: function(event){
-	    	$(event.target).parents('.slider_in_text').find('.thumbs .slide a').removeClass('active')
-	    	$(event.target).parents('.slider_in_text').find('.thumbs .slide:eq('+ event.item.index +') a').addClass('active')
-	    }
+		onTranslate: function (event) {
+			$(event.target).parents('.slider_in_text').find('.thumbs .slide a').removeClass('active')
+			$(event.target).parents('.slider_in_text').find('.thumbs .slide:eq(' + event.item.index + ') a').addClass('active')
+		}
 	})
 
 	$('.text_block .slider_in_text .thumbs .slider').owlCarousel({
@@ -33,30 +33,60 @@ $(function(){
 		dots: false,
 		smartSpeed: 500,
 		responsive: {
-			0:{
-	            items: 2
-	        },
-	        414:{
-	            items: 3
-	        },
-	        768:{
-	            items: 4
-	        }
+			0: {
+				items: 2
+			},
+			414: {
+				items: 3
+			},
+			768: {
+				items: 4
+			}
 		}
 	})
 
-	$('.text_block .slider_in_text .thumbs .slide a').click(function(e) {
+	$('.text_block .slider_in_text .thumbs .slide a').click(function (e) {
 		e.preventDefault()
 
 		$(this).parents('.slider_in_text').find('.thumbs .slide a').removeClass('active')
 		$(this).addClass('active')
 
-	    $(this).parents('.slider_in_text').find('.big .slider').trigger('to.owl.carousel', $(this).attr('data-slide-index'))
+		$(this).parents('.slider_in_text').find('.big .slider').trigger('to.owl.carousel', $(this).attr('data-slide-index'))
+	})
+
+
+	// Карусель товаров
+	$('.products .slider').owlCarousel({
+		margin: 20,
+		loop: false,
+		nav: true,
+		dots: false,
+		smartSpeed: 500,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1024: {
+				items: 3
+			},
+			1280: {
+				items: 4
+			}
+		},
+		onInitialized: event => {
+			productHeight($(event.target), $(event.target).find('.slide').length)
+		},
+		onTranslate: event => {
+			productHeight($(event.target), $(event.target).find('.slide').length)
+		}
 	})
 
 
 	// Переключение типа пользователя в форме
-	$('body').on('click', '.form .type label', function() {
+	$('body').on('click', '.form .type label', function () {
 		let content = $(this).data('content')
 		let parent = $(this).closest('.form')
 
@@ -66,7 +96,7 @@ $(function(){
 
 
 	// Товар в корзину
-	$('body').on('click', '.products .product .buy_link', function(e) {
+	$('body').on('click', '.products .product .buy_link', function (e) {
 		e.preventDefault()
 
 		// Здесь какие-то действия по добавлению товара в корзину
@@ -74,76 +104,76 @@ $(function(){
 
 
 	// Личный кабинет
-	$('.lk .personal .edit_personal').click(function(e){
-	    e.preventDefault()
+	$('.lk .personal .edit_personal').click(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.personal')
+		let parent = $(this).closest('.personal')
 
-	    parent.find('.info, .form').hide()
-	    parent.find('.personal_form').fadeIn()
+		parent.find('.info, .form').hide()
+		parent.find('.personal_form').fadeIn()
 
-	    parent.find('.links a').removeClass('active')
-	    $(this).addClass('active')
+		parent.find('.links a').removeClass('active')
+		$(this).addClass('active')
 	})
 
 
-	$('.lk .personal .edit_password').click(function(e){
-	    e.preventDefault()
+	$('.lk .personal .edit_password').click(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.personal')
+		let parent = $(this).closest('.personal')
 
-	    parent.find('.info, .form').hide()
-	    parent.find('.password_form').fadeIn()
+		parent.find('.info, .form').hide()
+		parent.find('.password_form').fadeIn()
 
-	   	parent.find('.links a').removeClass('active')
-	    $(this).addClass('active')
+		parent.find('.links a').removeClass('active')
+		$(this).addClass('active')
 	})
 
 
-	$('.lk .personal .form .cancel').click(function(e){
-	    e.preventDefault()
+	$('.lk .personal .form .cancel').click(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.personal')
+		let parent = $(this).closest('.personal')
 
-	    parent.find('.form').hide()
-	    parent.find('.info').fadeIn()
+		parent.find('.form').hide()
+		parent.find('.info').fadeIn()
 	})
 
 
-	$('.lk .addresses .adres_block .edit_link').click(function(e){
-	    e.preventDefault()
+	$('.lk .addresses .adres_block .edit_link').click(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.adres_block')
+		let parent = $(this).closest('.adres_block')
 
-	    if( $(this).hasClass('active') ){
-	        $(this).removeClass('active')
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
 
-	       	parent.find('.form').hide()
-	    	parent.find('.info').fadeIn()
-	    }else{
-	        $(this).addClass('active')
+			parent.find('.form').hide()
+			parent.find('.info').fadeIn()
+		} else {
+			$(this).addClass('active')
 
-	       	parent.find('.info').hide()
-	    	parent.find('.form').fadeIn()
-	    }
+			parent.find('.info').hide()
+			parent.find('.form').fadeIn()
+		}
 	})
 
-	$('.lk .addresses .adres_block form').submit(function(e){
-	    e.preventDefault()
+	$('.lk .addresses .adres_block form').submit(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.adres_block')
+		let parent = $(this).closest('.adres_block')
 
-	    parent.find('.edit_link').removeClass('active')
-	    parent.find('.form').hide()
-	    parent.find('.info').fadeIn()
+		parent.find('.edit_link').removeClass('active')
+		parent.find('.form').hide()
+		parent.find('.info').fadeIn()
 	})
 
-	$('.lk .addresses .adres_block .delete_link').click(function(e){
-	    e.preventDefault()
+	$('.lk .addresses .adres_block .delete_link').click(function (e) {
+		e.preventDefault()
 
-	    let parent = $(this).closest('.adres_block')
+		let parent = $(this).closest('.adres_block')
 
-	    parent.remove()
+		parent.remove()
 	})
 
 
@@ -155,27 +185,27 @@ $(function(){
 
 
 	// Добавление товара в корзину
-	$('body').on('click', '.buy_link', function(e) {
+	$('body').on('click', '.buy_link', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close()
 
 		$.fancybox.open({
-			src  : '#cart_success_modal',
-			type : 'inline'
+			src: '#cart_success_modal',
+			type: 'inline'
 		})
 	})
 
 
 	// Отправка форм
-	$('body').on('submit', '.form.ajax_submit', function(e) {
+	$('body').on('submit', '.form.ajax_submit', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close()
 
 		$.fancybox.open({
-			src  : '#success_modal',
-			type : 'inline'
+			src: '#success_modal',
+			type: 'inline'
 		})
 	})
 
@@ -188,36 +218,36 @@ $(function(){
 		dots: false,
 		smartSpeed: 500,
 		responsive: {
-			0:{
-	            items: 1
-	        },
-	        414:{
-	            items: 2
-	        },
-	        768:{
-	            items: 3
-	        },
-	        1024:{
-	            items: 4
-	        }
+			0: {
+				items: 1
+			},
+			414: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1024: {
+				items: 4
+			}
 		}
 	})
 })
 
 
 
-$(window).load(function(){
+$(window).load(function () {
 	// Доствака - Как мы работаем?
-	$('.delivery_steps .flex').each(function(){
+	$('.delivery_steps .flex').each(function () {
 		deliveryHeight($(this), parseInt($(this).css('--delivery_steps_count')))
 	})
 })
 
 
 
-$(window).resize(function(){
+$(window).resize(function () {
 	// Доствака - Как мы работаем?
-	$('.delivery_steps .flex').each(function(){
+	$('.delivery_steps .flex').each(function () {
 		deliveryHeight($(this), parseInt($(this).css('--delivery_steps_count')))
 	})
 })
@@ -225,19 +255,38 @@ $(window).resize(function(){
 
 
 // Доствака - Как мы работаем?
-function deliveryHeight(context, step){
+function deliveryHeight(context, step) {
 	let start = 0
 	let finish = step
 	let steps = context.find('.item')
 
 	steps.find('.name').height('auto')
 
-	for( let i = 0; i < steps.length; i++ ){
+	for (let i = 0; i < steps.length; i++) {
 		let obj = steps.slice(start, finish).find('.name')
 
-		setHeight( obj )
+		setHeight(obj)
 
-		start = start+step
-		finish = finish+step
+		start = start + step
+		finish = finish + step
+	}
+}
+
+
+// Выравнивание товаров
+function productHeight(context, step) {
+	let start = 0
+	let finish = step
+	let products = context.find('.product')
+
+	products.find('.name').height('auto')
+
+	for (let i = 0; i < products.length; i++) {
+		let obj = products.slice(start, finish).find('.name')
+
+		setHeight(obj)
+
+		start = start + step
+		finish = finish + step
 	}
 }
